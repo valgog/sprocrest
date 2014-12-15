236a010547f11e0674a3ee9b5cb9d5017d47092c
Alexander Kukushkin <alexander.kukushkin@zalando.de>
2014-12-15 15:33:17 +0100

    Add bash script which creates and populates test databases

---
+++ test/bootstrap.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+readonly owner="sproctest"
+readonly databases="sproctest sproctest1 sproctest2"
+
+readonly testdir="$( dirname $0 )"
+cd "${testdir}"
+
+function create_role() {
+    local role=$1
+    echo "CREATE role ${role} WITH LOGIN;"
+}
+
+function get_sources() {
+    find database -type f -name '*.sql' | sort | xargs cat
+}
+
+function bootstrap_database() {
+    local db=$1
+    cat << EOF
+RESET role;
+CREATE DATABASE ${db} owner ${owner};
+\connect ${db}
+SET role TO ${owner};
+EOF
+    get_sources
+}
+
+function get_bootstrap_script() {
+    create_role $owner
+    for db in $databases; do
+        bootstrap_database $db
+    done
+}
+
+get_bootstrap_script | psql -h localhost -U postgres -d postgres -1
